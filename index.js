08a604b98d2ba32072714775308639a9de89ed24
Tyler Hardy <tylerthardy@gmail.com>
2020-04-26 16:07:31 -0500

    Move google services to google folder

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const auth = require('./auth/auth');
-const appScript = require('./app-script/app-script');
+const auth = require('./google/auth/auth');
+const appScript = require('./google/app-script/app-script');
 
 auth.authorize((auth) => {
   appScript.call(auth);
