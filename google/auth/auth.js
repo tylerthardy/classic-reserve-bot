b334077217724c53572e1ccfbb21712dab3e3da9
Tyler Hardy <tylerthardy@gmail.com>
2020-05-05 09:56:35 -0500

    Cleanup exports for auth

--- google/auth/auth.js
+++ google/auth/auth.js
@@ -66,12 +66,13 @@ function getAccessToken(oAuth2Client, callback) {
     });
 }
 
-exports.authorize = (callback) => {
-    // Load client secrets from a local file.
-    fs.readFile('credentials.json', (err, content) => {
-        if (err) return console.log('Error loading client secret file:', err);
-        // Authorize a client with credentials, then call the Google Apps Script API.
-        authorize(JSON.parse(content), callback);
-    });
+module.exports = {
+    authorize: (callback) => {
+        // Load client secrets from a local file.
+        fs.readFile('credentials.json', (err, content) => {
+            if (err) return console.log('Error loading client secret file:', err);
+            // Authorize a client with credentials, then call the Google Apps Script API.
+            authorize(JSON.parse(content), callback);
+        });
+    }
 }
-
